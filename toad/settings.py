d05d99301242dd4425a3eeea4116cf6fc31eb767
RoyKeane94 <thomasbarratt@yahoo.com>
2025-06-16 20:28:25 +0100

    .env file and git ignore

--- toad/settings.py
+++ toad/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-td7)($7n9h6=0-r*-eqj*)i_c$oj$578adc@t5^3$1#0!u-o&i'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-key-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
