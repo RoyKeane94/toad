ada8f272595dce4404543d4608b0ff2087b6e27b
RoyKeane94 <thomasbarratt@yahoo.com>
2025-06-20 10:13:29 +0100

    Modal faster loading

--- theme/static_src/postcss.config.js
+++ theme/static_src/postcss.config.js
@@ -1,7 +1,32 @@
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    "postcss-simple-vars": {},
-    "postcss-nested": {}
-  },
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    '../../**/*.html',
+    '../../../templates/**/*.html',
+    '../../../**/templates/**/*.html',
+  ],
+  defaultExtractor: content => content.match(/[\w-./:]+(?<!:)/g) || []
+});
+
+const plugins = {
+  '@tailwindcss/postcss': {},
+  'postcss-simple-vars': {},
+  'postcss-nested': {},
+};
+
+if (process.env.NODE_ENV === 'production') {
+  plugins['@fullhuman/postcss-purgecss'] = {
+    content: [
+      './pages/templates/**/*.html',
+      './accounts/templates/**/*.html',
+      './theme/templates/**/*.html',
+      '../../pages/templates/**/*.html',
+      '../../accounts/templates/**/*.html',
+      '../../theme/templates/**/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  };
 }
+
+module.exports = {
+  plugins: plugins
+};
