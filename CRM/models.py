bf393499b6ed0ab42a220d137a4347d5b627f510
RoyKeane94 <thomasbarratt@yahoo.com>
2025-09-11 21:52:23 +0100

    Universities in alphabetic order

--- CRM/models.py
+++ CRM/models.py
@@ -38,8 +38,14 @@ class SocietyUniversity(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['name']
+    
     def __str__(self):
         return self.name
+
+    
 
 class LeadFocus(models.Model):
     name = models.CharField(max_length=20)
