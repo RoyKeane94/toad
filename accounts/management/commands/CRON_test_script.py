63c212937b8a2f3221af3844383593df40b3c079
RoyKeane94 <thomasbarratt@yahoo.com>
2025-09-14 20:01:48 +0100

    Adapting CRON_test_script

--- accounts/management/commands/CRON_test_script.py
+++ accounts/management/commands/CRON_test_script.py
@@ -1,1 +1,9 @@
-print ("CRON working")
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Test CRON functionality'
+
+    def handle(self, *args, **options):
+        self.stdout.write('CRON working')
+        self.stdout.write(self.style.SUCCESS('✓ CRON test completed successfully!'))
